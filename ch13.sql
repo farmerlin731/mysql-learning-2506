553d087617e61cb380f8c762c5c1ad7faa227565
farmerlin731 <69831413+farmerlin731@users.noreply.github.com>
2025-08-02 22:51:35 +0800

    Chpt13 finished! @ 250802

--- ch13.sql
+++ ch13.sql
@@ -74,3 +74,27 @@ select * from orders;
 -- |  5 | 2001-11-11 | 205.01 |           4 |
 -- |  6 | 2025-07-25 |  66.66 |           4 |
 -- +----+------------+--------+-------------+
+
+-- Questions & Answers
+select first_name,last_name, count(*) as total ,ROUND(AVG(imdb_score),1) as imdv_avg 
+from directors inner join movies where movies.director_id = directors.id 
+group by directors.id;
+-- +-------------+-----------+-------+----------+
+-- | first_name  | last_name | total | imdv_avg |
+-- +-------------+-----------+-------+----------+
+-- | Christopher | Nolan     |     4 |      8.7 |
+-- | Anthony     | Russo     |     1 |      8.2 |
+-- | Doug        | Liman     |     1 |      7.9 |
+-- | Andrew      | Stanton   |     1 |      8.4 |
+-- | Dean        | DeBlois   |     1 |      8.2 |
+-- | Pete        | Docter    |     2 |      8.3 |
+-- | James       | Gunn      |     1 |      8.1 |
+-- | Peter       | Jackson   |     2 |      7.9 |
+-- | Bryan       | Singer    |     1 |        8 |
+-- | Lee         | Unkrich   |     1 |      8.3 |
+-- | Don         | Hall      |     1 |      7.9 |
+-- | Joss        | Whedon    |     1 |      8.1 |
+-- | Jon         | Favreau   |     1 |      7.9 |
+-- | James       | Cameron   |     1 |      7.9 |
+-- | Nathan      | Greno     |     1 |      7.8 |
++-------------+-----------+-------+----------+
