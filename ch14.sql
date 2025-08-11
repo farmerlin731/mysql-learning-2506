b957d6138b5eb039f7b0d383101537ca22acb689
farmerlin731 <69831413+farmerlin731@users.noreply.github.com>
2025-08-11 16:51:06 +0800

    Chpt14 - Quest 4. - FINISHED. :)

--- ch14.sql
+++ ch14.sql
@@ -53,3 +53,32 @@ group by reviewers.id;
 -- | Pinkie     | Petit     |       7.25 |
 -- | Marlon     | Crafford  |       NULL |
 -- +------------+-----------+------------+
+
+-- Question 4
+select 
+    first_name,
+    last_name,
+    COUNT(rating) as COUNT,
+    MIN(IFNULL(rating,0)) as MIN,
+    MAX(IFNULL(rating,0)) as MAX,
+    ROUND(IFNULL(AVG(rating),0),2) as AVG,
+    -- CASE
+    --     WHEN COUNT(rating) > 0 THEN 'ACTIVE'
+    --     ELSE 'INACTIVE'
+    -- END as STATUS
+    IF(COUNT(rating) > 0 ,'ACTIVE','INACTIVE') as NEW_STATUS
+from reviewers 
+left join reviews on reviewers.id = reviews.reviewer_id 
+group by reviewers.id
+order by AVG desc;
+-- +------------+-----------+-------+------+------+------+------------+
+-- | first_name | last_name | COUNT | MIN  | MAX  | AVG  | NEW_STATUS |
+-- +------------+-----------+-------+------+------+------+------------+
+-- | Colt       | Steele    |    10 |  4.5 |  9.9 | 8.77 | ACTIVE     |
+-- | Thomas     | Stoneman  |     5 |  7.0 |  9.5 | 8.02 | ACTIVE     |
+-- | Kimbra     | Masters   |     7 |  6.8 |  9.0 | 7.86 | ACTIVE     |
+-- | Domingo    | Cortes    |     8 |  5.8 |  9.1 | 7.78 | ACTIVE     |
+-- | Wyatt      | Skaggs    |     8 |  5.5 |  9.3 | 7.71 | ACTIVE     |
+-- | Pinkie     | Petit     |     4 |  4.3 |  8.8 | 7.25 | ACTIVE     |
+-- | Marlon     | Crafford  |     0 |  0.0 |  0.0 | 0.00 | INACTIVE   |
+-- +------------+-----------+-------+------+------+------+------------+
