defe58f17862a5037ec2a075b54860c1fd30f67d
farmerlin731 <69831413+farmerlin731@users.noreply.github.com>
2026-02-28 21:49:39 +0800

    example until 45:32

--- tsmc-02-0227.sql
+++ tsmc-02-0227.sql
@@ -1,3 +1,53 @@
 -- Link: lukeb.co/sql_jobs_db
 -- Short cut : ctrl + enter
 SELECT * FROM job_postings_fact;
+
+-- DISTINCT
+SELECT DISTINCT
+	job_title_short
+FROM job_postings_fact
+
+-- WHERE(filter)
+SELECT DISTINCT
+	job_title_short,job_location,job_via,salary_year_avg
+FROM job_postings_fact
+WHERE salary_year_avg > 90000;
+
+-- Multi-Line comment /* something */
+
+-- ORDER BY (null is the smallest) (ASC/DESC)
+SELECT DISTINCT
+	job_title_short,job_location,job_via,salary_year_avg
+FROM job_postings_fact
+WHERE salary_year_avg > 100000
+ORDER BY salary_year_avg ASC
+LIMIT 100;
+
+-- NOT & '<>'
+SELECT DISTINCT
+	job_title_short,job_location,job_via,salary_year_avg
+FROM job_postings_fact
+WHERE NOT job_via <> 'via Indeed'
+ORDER BY salary_year_avg
+LIMIT 100;
+
+-- AND / OR / BETWEEN for combining condition
+-- Use () to control conditions!
+SELECT DISTINCT
+	job_title_short,job_location,job_via,salary_year_avg
+FROM job_postings_fact
+WHERE  
+	salary_year_avg BETWEEN 100000 AND 200000
+	AND job_via = 'via Indeed'
+ORDER BY salary_year_avg;
+
+
+-- IN
+SELECT DISTINCT
+	job_title_short,job_location,job_via,salary_year_avg
+FROM job_postings_fact
+WHERE  
+	job_title_short IN ('Data Scientist','Cloud Engineer')
+ORDER BY salary_year_avg;
+
+-- END @ 45:32
