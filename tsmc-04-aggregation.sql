38943ec1b03d061cea8d02efb014d2d23476f4f9
farmerlin731 <69831413+farmerlin731@users.noreply.github.com>
2026-03-01 20:56:33 +0800

    04-aggregation

---
+++ tsmc-04-aggregation.sql
@@ -0,0 +1,43 @@
+-- Aggregation : 聚合
+-- sum / count / avg / max / min ()
+-- used in group by / having
+
+-- basic
+SELECT 
+	SUM(salary_year_avg) AS salary_sum,
+    COUNT(*) AS count_rows,
+    COUNT(DISTINCT job_title_short) AS distinct_jobs,
+    MAX(salary_year_avg) AS max_salary
+FROM 
+	job_postings_fact
+WHERE
+	job_title_short = 'Data Analyst';
+
+
+-- Plus GROUP BY
+SELECT 
+    job_title AS title,
+    AVG(salary_year_avg) AS salary_avg,
+    MAX(salary_year_avg) AS salary_max
+FROM 
+	job_postings_fact
+GROUP BY
+	job_title_short
+ORDER BY
+	salary_avg DESC;
+
+-- HAVING
+-- u can't use aggregate function in 'WHERE' 
+SELECT 
+    job_title AS title,
+    COUNT(job_title_short) AS job_count,
+    AVG(salary_year_avg) AS salary_avg,
+    MAX(salary_year_avg) AS salary_max
+FROM 
+	job_postings_fact
+GROUP BY
+	job_title_short
+HAVING
+	salary_avg > 100000
+ORDER BY
+	salary_avg DESC;
